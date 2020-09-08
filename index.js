fe184b006175346d01187c78c444be2810ff9214
sean-slaughter <sthompson2822@gmail.com>
2020-09-07 22:14:17 -0500

    Add to index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+const express = require('express');
+
+const app = express();
+//tells the app controller where to look for views to render
+app.use(express.static(__dirname + "/public/"))
+//sets up root route
+app.get("/", function(req, resp){
+    resp.sendFile("index.html");
+})
+//listens on port 3000
+app.listen('3000', function(){
+    console.log('listening on port 3000')
+})
